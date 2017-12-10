5cf028377b84d49e85c1ac580060ce444e8d6e6f
SoonKhen OwYong <dude@owyong.sk>
2017-12-10 15:05:17 +0800

    added better readme, more comments on the code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function click_element(elems, text) {
+function click_element(elems, text) { // Searches elements and clicks when innerText matches
   for (var i = 0; i < elems.length; i++) {
     if (elems[i].innerText == text) {
       elems[i].click();
@@ -6,7 +6,10 @@ function click_element(elems, text) {
     }
   }
 }
-document.getElementsByClassName("ytp-settings-button")[0].click()
-document.getElementById('ytp-id-18').getElementsByClassName("ytp-menuitem")[3].click()
-click_element(document.getElementById('ytp-id-18').getElementsByClassName("ytp-menuitem-label"), "Traducción automática")
-click_element(document.getElementById('ytp-id-18').getElementsByClassName("ytp-menuitem-label"), "Español")
+
+// Step by step of clicking each UI element in YouTube video
+document.getElementsByClassName("ytp-settings-button")[0].click();
+document.getElementById('ytp-id-18').getElementsByClassName("ytp-menuitem")[3].click();
+// document.getElementById('ytp-id-17').getElementsByClassName("ytp-menuitem")[3].click();
+click_element(document.getElementById('ytp-id-18').getElementsByClassName("ytp-menuitem-label"), "Traducción automática");
+click_element(document.getElementById('ytp-id-18').getElementsByClassName("ytp-menuitem-label"), "Español");
